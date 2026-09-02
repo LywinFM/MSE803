47ee45e2431ed49f374856c461959d4f0d4e7d7f
Darling Lywin Manalang <270535434@yoobeestudent.ac.nz>
2026-09-02 22:49:08 +1200

    Stage now

---
+++ icon.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+        create_icon(
+        base_size=1024,
+        bg_color="#FF5722",
+        text="AB",
+        font_path=None  # You can specify a .ttf font file path here
+    )
